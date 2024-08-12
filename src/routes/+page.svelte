1b969f16c127d4963d01f1704974fcef11125424
sakura-aoi-ororora <souyakoji@gmail.com>
2024-08-12 11:29:36 +0900

    それっぽいインターフェース

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import {Label, Input, Card, Button} from "flowbite-svelte";
+</script>
+
+<div class="m-5">
+    <Label for="input" class="block mb-2">元の文字列</Label>
+    <Input id="input" placeholder="絵文字でもなんでも"/>
+</div>
+
+<div class="m-5">
+    <Label for="output" class="block mb-2">完成品</Label>
+    <Card id="output">
+        <h1 class="text-3xl">文字</h1>
+        <Button size="xs" class="w-fit mt-2">Copy</Button>
+    </Card>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+import flowbitePlugin from 'flowbite/plugin'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ["./src/**/*.{html,js,svelte,ts}",'./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}'],
+  darkMode: "selector",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: { "50": "#f7fee7", "100": "#ecfccb", "200": "#d9f99d", "300": "#bef264", "400": "#a3e635", "500": "#84cc16", "600": "#65a30d", "700": "#4d7c0f", "800": "#3f6212", "900": "#365314" }
+      }
+    },
   },
-  plugins: [],
+  plugins: [flowbitePlugin],
 }
 
